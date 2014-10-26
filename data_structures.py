52f9964bd1553b4c411ccda1a531bdd3ba4e81bd
Marc-André Legault <legaultmarc@gmail.com>
2014-10-26 18:47:33 -0400

    Refactored the variant data structures to include a parent Variant class and and Indel subclass (with SNP as another Variant subclass).

--- data_structures.py
+++ data_structures.py
@@ -5,11 +5,232 @@ import contextlib
 import json
 import logging
 
+import sys
+import traceback
+
 import settings
 
-__all__ = ["SNP", ]
+__all__ = ["SNP", "Indel", "Variant"]
 
-class SNP(object):
+class Variant(object):
+
+    def __init__(self, *args, **kwargs):
+        """Super constructor for Variant objects. 
+
+        It will initalize the object by assuming one of the following
+        scenarios:
+        
+        1) The position arguments (*args) correspond to the elements of the
+           _PARAMETERS argument which is overriden by the subclasses when the
+           parent constructor is called.
+
+        2) All of the _PARAMETERS elements should be specified as keyword
+           arguments (**kwargs).
+
+        A sanity check will be done after initalization to make sure that all
+        of the parameters are defined.
+
+        """
+
+        if "_PARAMETERS" in kwargs:
+            _PARAMETERS = kwargs.pop("_PARAMETERS")
+        else:
+            _PARAMETERS = ["chrom", "start", "end"]
+
+        if len(args) == len(_PARAMETERS):
+            for i, p in enumerate(_PARAMETERS):
+                setattr(self, p, args[i])
+        else:
+            for k in kwargs.keys():
+                if k not in _PARAMETERS:
+                    raise Exception("Unknown parameter {}.".format(k))
+
+                setattr(self, k, kwargs.pop(k))
+
+        # Make sure everything was set.
+        for p in _PARAMETERS:
+            if getattr(self, p, "-1") == "-1":
+                raise Exception("Missing parameter {}.".format(p))
+
+    def vcf_header(self):
+        """Returns a valid VCF header line. 
+        
+        """
+        return "\t".join([
+            "#CHROM",
+            "POS",
+            "ID",
+            "REF",
+            "ALT",
+            "QUAL",
+            "FILTER",
+            "INFO",
+        ])
+
+    def get_position(self):
+        """Abstract method.
+
+        Returns the genomic position of the variant using the standard format:
+        chrXX:00000-11111.
+
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def from_ensembl_api(cls, rs, build=settings.BUILD):
+        """Builds the correct Variant subclass for the specified rs number.
+
+        :param rs: The rs number describing the variant of interest.
+        :type rs: str
+
+        :param build: The genome build (either GRCh37 or GRCh38, default is 
+                      defined in the settings.
+        :type build: str
+
+        This methos will fetch the data from the Ensembl rest api and try
+        to guess the right Variant subclass (e.g. SNP or Indel).
+
+        """
+
+        if build == "GRCh37":
+            url = ("http://grch37.rest.ensembl.org/variation/homo_sapiens/{snp}"
+                   "?content-type=application/json")
+        elif build == "GRCh38":
+            url = ("http://rest.ensembl.org/variation/homo_sapiens/{snp}"
+                   "?content-type=application/json")
+        else:
+            raise Exception("Unknown build '{}'.".format(build))
+
+        url = url.format(snp=rs)
+        try:
+            with contextlib.closing(urllib2.urlopen(url)) as stream:
+                response = json.load(stream)
+        except urllib2.HTTPError:
+            logging.warning("Request '{}' failed.".format(url))
+            return None
+
+        pos = None
+        for mapping in response["mappings"]:
+            if mapping["assembly_name"] == build:
+                # This is a SNP 
+                if mapping["start"] == mapping["end"]:
+                    pos = "chr{}_{}".format(
+                        mapping["location"].split("-")[0],
+                        mapping["allele_string"]
+                    )
+
+                    # Note that for multi (>2) allelic loci, this will return
+                    # a list of variants.
+                    return SNP.from_str(pos, rs=rs)
+
+                # Otherwise an Indel
+                else:
+                    chrom = str(mapping["seq_region_name"])
+                    start = mapping["start"]
+                    end = mapping["end"]
+                    alleles = str(mapping["allele_string"]).split("/")
+                    ref = alleles[0]
+                    alts = alleles[1:]
+                    if len(alts) > 1:
+                        indels = []
+                        for alt in alts:
+                            indel = Indel(chrom, start, end, rs, ref, alt)
+                            indels.append(indel)
+                        return indels
+                    else:
+                        return Indel(chrom, start, end, rs, ref, alts[0])
+
+
+class Indel(Variant):
+    def __init__(self, *args, **kwargs):
+        """Initialize an Indel.
+
+        Either call this method with the parameters corresponding to:
+        ``chrom, start, end, rs, ref, alt`` or by using the corresponding
+        named parameters.
+
+        """
+
+        _PARAMETERS = [
+            "chrom", 
+            "start",
+            "end",
+            "rs",
+            "ref",
+            "alt",
+        ]
+        kwargs["_PARAMETERS"] = _PARAMETERS
+        super(Indel, self).__init__(*args, **kwargs)
+
+        self.start = int(self.start)
+        self.end = int(self.end)
+        try:
+            assert re.match(r"([0-9]{1,2}|MT|X|Y)", str(self.chrom))
+            assert self.rs is None or re.match(r"^rs[0-9]+$", self.rs)
+            assert type(self.start) is int and type(self.end) is int
+            assert self.start < self.end
+            assert type(self.ref) is str
+            assert type(self.alt) is str
+        except AssertionError as e:
+            logging.critical(
+                "Assertion failed constructing the Indel object. \n"
+                "Parameters were: \n" 
+                "\tchrom: {chrom} ({chrom_type})\n"
+                "\tstart: {start} ({start_type})\n"
+                "\tstart: {end} ({end_type})\n"
+                "\trs: {rs} ({rs_type})\n"
+                "\tref: {ref} ({ref_type})\n"
+                "\talt: {alt} ({alt_type})\n".format(
+                    chrom=self.chrom, chrom_type=type(self.chrom),
+                    start=self.start, start_type=type(self.start),
+                    end=self.end, end_type=type(self.end),
+                    rs=self.rs, rs_type=type(self.rs),
+                    ref=self.ref, ref_type=type(self.ref),
+                    alt=self.alt, alt_type=type(self.alt),
+                )
+            )
+            traceback.print_tb(sys.exc_info()[2])
+            raise e
+
+    def get_position(self):
+        """Returns an indel in the standard chrXX:START-END notation. 
+        
+        """
+        return "chr{}:{}-{}".format(self.chrom, self.start, self.end)
+
+    def vcf_line(self):
+        """Returns a line describing the current variant as expected by the VCF format. 
+
+        """
+        return "\t".join(str(i) for i in [
+            self.chrom,
+            self.start,
+            self.rs if self.rs else ".",
+            self.ref,
+            self.alt,
+            ".", # No Quality
+            "PASS", # PASS for filter
+            ".", # Info
+        ])
+
+    def __repr__(self):
+        return "chr{}:{}_{}/{}".format(
+            self.chrom, self.start, self.ref, self.alt
+        )
+
+    @classmethod
+    def from_ensembl_api(cls, rs, build=settings.BUILD):
+        """Gets the information from the Ensembl REST API.
+
+        :param rs: The rs number for the variant of interest.
+        :param build: The genome build (e.g. GRCh37 or GRCh38).
+
+        """
+        variants = Variant.from_ensembl_api(rs, build)
+        return [v for v in variants if v.__class__ is cls]
+
+
+class SNP(Variant):
     def __init__(self, *args, **kwargs):
         """Initialize a SNP. 
 
@@ -18,30 +239,17 @@ class SNP(object):
 
         """
 
-        parameters = [
+        _PARAMETERS = [
             "chrom", 
             "pos",
             "rs",
             "ref",
             "alt",
         ]
+        kwargs["_PARAMETERS"] = _PARAMETERS
+        super(SNP, self).__init__(*args, **kwargs)
 
-        if len(args) == 5:
-            # Initialize assuming (chrom, pos, rs, ref, alt)
-            for i, p in enumerate(parameters):
-                setattr(self, p, args[i])
-        else:
-            for k in kwargs.keys():
-                if k not in parameters:
-                    raise Exception("Unknown parameter {}.".format(k))
-
-                setattr(self, k, kwargs.pop(k))
-
-        # Normalize attributes.
-        for p in parameters:
-            if getattr(self, p, "-1") == "-1":
-                raise Exception("Missing parameter {}.".format(p))
-
+        # Make sure everthing has the right type.
         self.pos = int(self.pos)
         try:
             assert re.match(r"([0-9]{1,2}|MT|X|Y)", str(self.chrom))
@@ -64,24 +272,14 @@ class SNP(object):
                     alt=self.alt, alt_type=type(self.alt),
                 )
             )
+            traceback.print_tb(sys.exc_info()[2])
             raise e
 
     def get_position(self):
-        """Returns a variant in the standard chrXX:pos notation. """
+        """Returns a variant in the standard chrXX:pos notation.
+        
+        """
         return "chr{}:{}".format(self.chrom, self.pos)
-
-    def vcf_header(self):
-        """Returns a valid VCF header line. """
-        return "\t".join([
-            "#CHROM",
-            "POS",
-            "ID",
-            "REF",
-            "ALT",
-            "QUAL",
-            "FILTER",
-            "INFO",
-        ])
 
     def vcf_line(self):
         """Returns a line describing the current variant as expected by the VCF format. 
@@ -106,43 +304,8 @@ class SNP(object):
         :param build: The genome build (e.g. GRCh37 or GRCh38).
 
         """
-        if build == "GRCh37":
-            url = ("http://grch37.rest.ensembl.org/variation/homo_sapiens/{snp}"
-                   "?content-type=application/json")
-        elif build == "GRCh38":
-            url = ("http://rest.ensembl.org/variation/homo_sapiens/{snp}"
-                   "?content-type=application/json")
-        else:
-            raise Exception("Unknown build '{}'.".format(build))
-
-        url = url.format(snp=rs)
-        try:
-            with contextlib.closing(urllib2.urlopen(url)) as stream:
-                response = json.load(stream)
-        except urllib2.HTTPError:
-            logging.warning("Request '{}' failed.".format(url))
-            return None
-
-        pos = None
-        for mapping in response["mappings"]:
-            if mapping["assembly_name"] == build:
-                if mapping["start"] == mapping["end"]: # Only SNPs.
-                    pos = "chr{}_{}".format(
-                        mapping["location"].split("-")[0],
-                        mapping["allele_string"]
-                    )
-                else:
-                    # We have an indel. Ignoring.
-                    logging.warning("{} is not a SNP (ignoring).".format(
-                        response["name"],
-                    ))
-
-        if not pos:
-            raise Exception("Could not find all the information to create SNP "
-                "object from Ensembl.")
-
-        return SNP.from_str(pos, rs=rs)
-
+        variants = Variant.from_ensembl_api(rs, build)
+        return [v for v in variants if v.__class__ is cls]
 
     @classmethod
     def from_str(cls, s, rs=None):
