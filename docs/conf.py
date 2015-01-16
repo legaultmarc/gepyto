ba608affe110df268de425bac5351bb5bc2b5759
Marc-André Legault <legaultmarc@gmail.com>
2015-01-16 15:34:20 -0500

    Fixed docs for readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,18 @@ sys.path.insert(0,
     os.path.abspath('..')
 )
 
+# Read the docs configuration.
+if os.environ.get("READTHEDOCS", None) == "True":
+    from unittest.mock import MagicMock
+
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+                return Mock()
+
+    MOCK_MODULES = ['numpy', 'pandas']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
