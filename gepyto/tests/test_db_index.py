53a72a887cd9b0e2d207adb9259d23cd4f10ad7d
Marc-André Legault <legaultmarc@gmail.com>
2015-04-02 14:27:04 -0400

    Added some more tests for the index.

--- gepyto/tests/test_db_index.py
+++ gepyto/tests/test_db_index.py
@@ -32,19 +32,18 @@ class TestIndex(unittest.TestCase):
         cls.fn = ".test_index_gepyto.txt"
         cls.f = open(cls.fn, "w")
 
+        # The third column is 1 for the first occurence of the locus and
+        # gibberish for the other columns.
         cls.positions = [
-            ("chr1", 1), (1, 11), (1, 123), (2, 1), (2, 11), (2, 11), (2, 11),
-            (2, 11), (2, 21), (2, 34), (3, 1), (3, 2), (3, 3), (3, 4), (3, 4),
-            (3, 5), ("chr3", 6), (3, 7), (3, 8), (3, 9), (4, 1), (5, 2),
-            ("X", 3), ("Y", 2),
+            ("chr1", 1, 1), (1, 11, 1), (1, 123, 1), (2, 1, 1), (2, 11, 1), 
+            (2, 11, "gibberish"), (2, 11, "blablatidoo"), (2, 11, "potatoes"),
+            (2, 21, 1), (2, 34, 1), (3, 1, 1), (3, 2, 1), (3, 3, 1), (3, 4, 1),
+            (3, 4, "banana"), (3, 5, 1), ("chr3", 6, 1), (3, 7, 1), (3, 8, 1),
+            (3, 9, 1), (4, 1, 1), (5, 2, 1), (5, 2, "duplicate"), ("X", 3, 1),
+            ("Y", 2, 1), ("Y", 2, 131), ("Y", 2, "duplicate_again"),
         ]
-        for chrom, pos in cls.positions:
-            # Generate a random third column.
-            n = random.randint(100, 125)
-            s = "".join(
-                [random.choice(string.ascii_lowercase) for i in range(n)]
-            )
-            cls.f.write("\t".join((str(chrom), str(pos), s)) + "\n")
+        for fields in cls.positions:
+            cls.f.write("\t".join([str(i) for i in fields]) + "\n")
 
         cls.f.close()
         cls.f = open(cls.fn, "r")
@@ -61,7 +60,7 @@ class TestIndex(unittest.TestCase):
 
         loci = TestIndex.positions
         random.shuffle(loci)
-        for chrom, pos in loci:
+        for chrom, pos, _ in loci:
             # We only test indexed chromosomes...
             if chrom in idx[0]["chrom_codes"]:
                 # Search the entry.
@@ -86,7 +85,7 @@ class TestIndex(unittest.TestCase):
 
         loci = TestIndex.positions
         random.shuffle(loci)
-        for chrom, pos in loci:
+        for chrom, pos, _ in loci:
             # Search the entry.
             self.assertTrue(
                 db.index.goto(TestIndex.f, idx, chrom, pos)
@@ -97,3 +96,21 @@ class TestIndex(unittest.TestCase):
             self.assertFalse(
                 db.index.goto(TestIndex.f, idx, chrom, pos)
             )
+
+    def test_repeated_regions(self):
+        db.index.build_index(TestIndex.fn, 0, 1, index_rate=0.8)
+        idx = db.index.get_index(TestIndex.fn)
+
+        loci = TestIndex.positions
+        random.shuffle(loci)
+        for chrom, pos, _ in loci:
+            try:
+                db.index.goto(TestIndex.f, idx, chrom, pos)
+                # The column should always be one.
+                line = TestIndex.f.readline().rstrip().split("\t")
+                self.assertEqual(line[0], str(chrom))
+                self.assertEqual(line[1], str(pos))
+                self.assertEqual(line[2], "1")
+
+            except db.index.ChromosomeNotIndexed:
+                pass
